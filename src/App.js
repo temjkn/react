c84d1f1f0c47c62f52f0e9bfb2ce555e0a02eb81
temjkn <temjkn@gmail.com>
2020-09-18 10:13:20 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Profile from './components/profile/Profile';
+import News from './components/news/News';
+import Setting from './components/setting/Setting';
+import { Route, BrowserRouter } from 'react-router-dom';
+import DialogsContainer from './components/dialogs/DialogsContainer';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app-wraper">
+        <Header/>
+        <Nav/>
+        <div className='content'>
+          <Route path="/profile" render={()=>
+            <Profile />
+          }
+          />
+          <Route path="/dialogs" render = { ()=> 
+            <DialogsContainer />
+          }
+          />
+          <Route path="/news" component={News}/>
+          <Route path="/Setting" component={Setting}/>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import store from './redux/redux-store';
+import { Provider } from 'react-redux';
 
+// функция, перерисовывает страницу при каждом вызове
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Provider store = {store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
